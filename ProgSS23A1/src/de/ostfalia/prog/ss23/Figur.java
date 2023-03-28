e658e2167abae01754b8196fa7f3931803aa9a30
id840556 <id840556@4d28ed4a-c127-4c07-8cc9-688a806b37f3>
2023-03-28 16:50:34 +0000

    .

--- ProgSS23A1/src/de/ostfalia/prog/ss23/Figur.java
+++ ProgSS23A1/src/de/ostfalia/prog/ss23/Figur.java
@@ -14,34 +14,42 @@ public class Figur {
     }
 
     public void setPosition(int position) {
+
         this.position = position;
     }
 
     public int getPosition() {
+
         return position;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setParadies(Boolean paradies) {
+
         this.paradies = paradies;
     }
 
     public boolean getParadies() {
+
         return paradies;
     }
 
     public void setWurf(int[] wurf) {
+
         this.wurf = wurf;
     }
 
     public int[] getWurf() {
+
         return wurf;
     }
 }
