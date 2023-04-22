4d98566d647af68f77d38d54c2c6c502de4a3a48
Oliver Greiner-Petter <contact@greiner-petter.net>
2023-04-22 18:24:14 +0000

    a3.4

--- ProgSS23A3/src/felder/Labyrinth.java
+++ ProgSS23A3/src/felder/Labyrinth.java
@@ -1,7 +1,5 @@
 package de.ostfalia.prog.ss23.felder;
 
-import de.ostfalia.prog.ss23.Figur;
-
 public class Labyrinth extends Feld{
     public Labyrinth(Feld davor, int position) {
         super(davor, position);
